4cdd8b4ad373a1297644fb8d43eef7d8a8566075
xpoveda <xpoveda@gmail.com>
2018-06-13 00:34:24 +0200

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Holaaaaaaaa Docker World";
+        return "Hello another time Docker World";
     }
 
     public static void main(String[] args) {
